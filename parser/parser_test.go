bb8115db20954e409e80bd2c01f6709f29b20713
rsb <rsb.code@gmail.com>
2019-10-11 09:36:29 -0700

    Finished ch2.3 Parsers first steps

---
+++ parser/parser_test.go
@@ -0,0 +1,44 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/rsb/monkey_interpreter/ast"
+	"github.com/rsb/monkey_interpreter/lexer"
+	"github.com/rsb/monkey_interpreter/parser"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLetStatements(t *testing.T) {
+	assert := assert.New(t)
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+	`
+
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	assert.NotNil(program)
+	assert.Equal(len(program.Statements), 3)
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		assert.Equal(stmt.TokenLiteral(), "let")
+		letStmt, ok := stmt.(*ast.LetStatement)
+		assert.True(ok)
+		assert.Equal(letStmt.Name.Value, tt.expectedIdentifier)
+		assert.Equal(letStmt.Name.TokenLiteral(), tt.expectedIdentifier)
+	}
+}

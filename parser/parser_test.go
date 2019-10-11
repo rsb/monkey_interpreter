cab7071e8d468a83b3b23317b7ffe3ae6c0aa843
rsb <rsb.code@gmail.com>
2019-10-11 10:19:44 -0700

    Finished ch2.4 Parsing LetStatements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -42,3 +42,23 @@ func TestLetStatements(t *testing.T) {
 		assert.Equal(letStmt.Name.TokenLiteral(), tt.expectedIdentifier)
 	}
 }
+
+func TestLetSTatementsWithErrors(t *testing.T) {
+	assert := assert.New(t)
+	input := `
+		let x 5;
+		let = 10;
+		let 838383;
+	`
+
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	assert.NotNil(program)
+
+	errList := p.Errors()
+	assert.Equal(len(errList), 3)
+	assert.Equal(errList[0], "expected next token to be =, got INT instead")
+	assert.Equal(errList[1], "expected next token to be IDENT, got = instead")
+	assert.Equal(errList[2], "expected next token to be IDENT, got INT instead")
+}
